5d599cc3325b79c2134bae90ef55564f5f1cd038
deviant <mb.deviant@gmail.com>
2023-02-21 00:21:56 +0300

    create functions to update display on each search

--- src/UI.js
+++ src/UI.js
@@ -1,5 +1,49 @@
 export default async function updateUI(promise) {
-    promise.then(function (data) {
-        console.log(data);
+    const weatherContainer = document.getElementById("weather-container");
+    console.log(promise);
+    promise.then(function (weatherData) {
+        console.log(
+            weatherData.city,
+            weatherData.desc,
+            weatherData.temp.toFixed(),
+            weatherData.temp_feels_like.toFixed(),
+            weatherData.wind,
+            weatherData.humidity
+        );
+        if (weatherContainer.querySelector("#weather-card")) {
+            const currentCard = weatherContainer.querySelector("#weather-card");
+            weatherContainer.removeChild(currentCard);
+        }
+        const weatherElement = createWeatherCard(weatherData);
+        weatherContainer.appendChild(weatherElement);
     });
+}
+
+function createWeatherCard(data) {
+    const container = document.createElement("div");
+    container.setAttribute("id", "weather-card");
+
+    const cityTitle = document.createElement("h2");
+    const desc = document.createElement("p");
+    desc.classList.add("capitalize");
+    const temp = document.createElement("p");
+    const tempFeelsLike = document.createElement("p");
+    const humidity = document.createElement("p");
+    const wind = document.createElement("p");
+
+    cityTitle.textContent = `${data.city}`;
+    desc.textContent = `${data.desc}`;
+    temp.textContent = ` Temperature: ${data.temp.toFixed()}`;
+    tempFeelsLike.textContent = `Feels like: ${data.temp_feels_like.toFixed()}`;
+    humidity.textContent = `Humidity: ${data.humidity}%`;
+    wind.textContent = `Wind: ${data.wind}`;
+
+    container.appendChild(cityTitle);
+    container.appendChild(desc);
+    container.appendChild(temp);
+    container.appendChild(tempFeelsLike);
+    container.appendChild(humidity);
+    container.appendChild(wind);
+
+    return container;
 }
