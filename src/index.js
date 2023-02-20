a37e1ce4d519b0de9ad15929d255ab143dfe434d
deviant <mb.deviant@gmail.com>
2023-02-21 00:21:22 +0300

    remove commented out lines

--- src/index.js
+++ src/index.js
@@ -7,13 +7,8 @@ const searchButton = document.getElementById("search-button");
 searchButton.addEventListener("click", () => {
     console.log(searchBar.value);
     const weatherPromise = getWeatherData(searchBar.value); //check for empty input & 404
-    console.log(updateUI(weatherPromise));
+    updateUI(weatherPromise);
 });
-// const weatherPromise = getWeatherData("talin");
-//404 not found
-// weatherPromise.then(function (data) {
-//     console.log(data.desc);
-// });
 
 //default unit is kelvin, display it as celcius first
 // make it convertable to fahrenheit too.
