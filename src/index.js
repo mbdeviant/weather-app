b4e870e4bfba29c3d96dbb4f888431ea38304399
deviant <mb.deviant@gmail.com>
2023-02-20 19:49:28 +0300

    import getWeatherData function

--- src/index.js
+++ src/index.js
@@ -1,26 +1,4 @@
-import { HIDDEN_ARTIFACT } from "./biggest-secret-in-the-world";
-
-async function getWeatherData(location) {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${HIDDEN_ARTIFACT}`;
-
-    try {
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error(`Error fetching the data :${response.status}`);
-        }
-        const data = await response.json();
-        const weather = {
-            desc: data.weather[0].description,
-            temp: data.main.temp,
-            humidity: data.main.humidity,
-            wind: data.wind.speed,
-        };
-        return weather;
-    } catch (error) {
-        console.log(error);
-        return null;
-    }
-}
+import getWeatherData from "./data-fetch";
 
 const weatherPromimse = getWeatherData("madrid");
 
