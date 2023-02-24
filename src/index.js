dba39420dabc96ec930f7e6708f5c98b69cea9b8
deviant <mb.deviant@gmail.com>
2023-02-24 21:58:28 +0300

    reset the searchbar value after submit

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ searchButton.addEventListener("click", () => {
     console.log(searchBar.value);
     const weatherPromise = getWeatherData(searchBar.value); //check for empty input & 404
     updateUI(weatherPromise);
+    searchBar.value = "";
 });
 
 searchBar.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
         const weatherPromise = getWeatherData(searchBar.value); //check for empty input & 404
         updateUI(weatherPromise);
+        searchBar.value = "";
     }
 });
