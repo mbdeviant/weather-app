d61a9c1e3db0d7017d4fa0d6b90a994c2bda467e
deviant <mb.deviant@gmail.com>
2023-02-27 02:30:02 +0300

    configure webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  devtool: "inline-source-map",
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    devtool: "inline-source-map",
 };
