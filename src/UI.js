99f87383ce3121e2fcc91544842e43878184d088
deviant <mb.deviant@gmail.com>
2023-08-07 16:21:10 +0300

    change weather titles to h element from p

--- src/UI.js
+++ src/UI.js
@@ -1,126 +1,131 @@
 export default async function updateUI(promise) {
-    const weatherContainer = document.getElementById("weather-container");
-    const searchBar = document.getElementById("searchbar");
+  const weatherContainer = document.getElementById("weather-container");
+  const searchBar = document.getElementById("searchbar");
 
-    promise.then(function (weatherData) {
-        if (!weatherData) return;
+  promise.then(function (weatherData) {
+    if (!weatherData) return;
 
-        if (weatherContainer.querySelector("#weather-card")) {
-            const currentCard = weatherContainer.querySelector("#weather-card");
-            weatherContainer.removeChild(currentCard);
-        }
-        const weatherElement = createWeatherCard(weatherData);
-        weatherContainer.appendChild(weatherElement);
-        searchBar.value = "";
-    });
+    if (weatherContainer.querySelector("#weather-card")) {
+      const currentCard = weatherContainer.querySelector("#weather-card");
+      weatherContainer.removeChild(currentCard);
+    }
+    const weatherElement = createWeatherCard(weatherData);
+    weatherContainer.appendChild(weatherElement);
+    searchBar.value = "";
+  });
 }
 
 function createWeatherCard(data) {
-    const container = document.createElement("div");
-    container.setAttribute("id", "weather-card");
+  const container = document.createElement("div");
+  container.setAttribute("id", "weather-card");
 
-    const cardHeader = document.createElement("div");
-    cardHeader.classList.add("weather-card-header");
-    const cityTitle = document.createElement("h2");
+  const cardHeader = document.createElement("div");
+  cardHeader.classList.add("weather-card-header");
+  const cityTitle = document.createElement("h1");
+  cityTitle.classList.add("city-title");
 
-    const celcius = document.createElement("p");
-    celcius.textContent = "°C";
-    celcius.classList.add("active");
-    const brace = document.createElement("p");
-    brace.textContent = "/";
-    const fahrenheit = document.createElement("p");
-    fahrenheit.textContent = "°F";
+  const celcius = document.createElement("p");
+  celcius.textContent = "°C";
+  celcius.classList.add("active");
+  const brace = document.createElement("p");
+  brace.textContent = "/";
+  const fahrenheit = document.createElement("p");
+  fahrenheit.textContent = "°F";
 
-    const tempToggle = document.createElement("div");
-    tempToggle.classList.add("temp-toggle");
-    tempToggle.appendChild(celcius);
-    tempToggle.appendChild(brace);
-    tempToggle.appendChild(fahrenheit);
+  const tempToggle = document.createElement("div");
+  tempToggle.classList.add("temp-toggle");
+  tempToggle.appendChild(celcius);
+  tempToggle.appendChild(brace);
+  tempToggle.appendChild(fahrenheit);
 
-    tempToggle.addEventListener("click", () => {
-        if (celcius.classList.contains("active")) {
-            celcius.classList.remove("active");
-            fahrenheit.classList.add("active");
+  tempToggle.addEventListener("click", () => {
+    if (celcius.classList.contains("active")) {
+      celcius.classList.remove("active");
+      fahrenheit.classList.add("active");
 
-            const tempF = convertToFahrenheit(data.temp.toFixed());
-            temp.textContent = ` Temperature: ${tempF}°F`;
-            const tempFeelslikeF = convertToFahrenheit(
-                data.temp_feels_like.toFixed()
-            );
-            tempFeelsLike.textContent = `Feels like: ${tempFeelslikeF}°F`;
-        } else if (fahrenheit.classList.contains("active")) {
-            fahrenheit.classList.remove("active");
-            celcius.classList.add("active");
-            temp.textContent = `Temperature: ${data.temp.toFixed()}°C`;
-            tempFeelsLike.textContent = `Feels like: ${data.temp_feels_like.toFixed()}°C`;
-        }
-    });
+      const tempF = convertToFahrenheit(data.temp.toFixed());
+      temp.textContent = ` Temperature: ${tempF}°F`;
+      const tempFeelslikeF = convertToFahrenheit(
+        data.temp_feels_like.toFixed()
+      );
+      tempFeelsLike.textContent = `Feels like: ${tempFeelslikeF}°F`;
+    } else if (fahrenheit.classList.contains("active")) {
+      fahrenheit.classList.remove("active");
+      celcius.classList.add("active");
+      temp.textContent = `Temperature: ${data.temp.toFixed()}°C`;
+      tempFeelsLike.textContent = `Feels like: ${data.temp_feels_like.toFixed()}°C`;
+    }
+  });
 
-    const desc = document.createElement("p");
-    desc.classList.add("capitalize");
-    const temp = document.createElement("p");
+  const desc = document.createElement("h2");
+  desc.classList.add("capitalize");
+  desc.classList.add("weather-info");
+  const temp = document.createElement("h2");
+  temp.classList.add("weather-info");
+  const tempFeelsLike = document.createElement("h2");
+  tempFeelsLike.setAttribute("id", "temp-feels-like");
+  tempFeelsLike.classList.add("weather-info");
+  const humidity = document.createElement("h2");
+  humidity.classList.add("weather-info");
+  const wind = document.createElement("h2");
+  wind.classList.add("weather-info");
 
-    const tempFeelsLike = document.createElement("p");
-    tempFeelsLike.setAttribute("id", "temp-feels-like");
-    const humidity = document.createElement("p");
-    const wind = document.createElement("p");
+  cityTitle.textContent = `${data.city}`;
+  desc.textContent = `${data.desc}`;
+  temp.textContent = ` Temperature: ${data.temp.toFixed()}°C`;
+  tempFeelsLike.textContent = `Feels like: ${data.temp_feels_like.toFixed()}°C`;
+  humidity.textContent = `Humidity: ${data.humidity}%`;
+  wind.textContent = `Wind: ${data.wind} m/s`;
 
-    cityTitle.textContent = `${data.city}`;
-    desc.textContent = `${data.desc}`;
-    temp.textContent = ` Temperature: ${data.temp.toFixed()}°C`;
-    tempFeelsLike.textContent = `Feels like: ${data.temp_feels_like.toFixed()}°C`;
-    humidity.textContent = `Humidity: ${data.humidity}%`;
-    wind.textContent = `Wind: ${data.wind} m/s`;
+  setBackground(data.desc);
 
-    setBackground(data.desc);
+  cardHeader.appendChild(cityTitle);
+  cardHeader.appendChild(tempToggle);
+  container.appendChild(cardHeader);
+  container.appendChild(desc);
+  container.appendChild(temp);
+  container.appendChild(tempFeelsLike);
+  container.appendChild(humidity);
+  container.appendChild(wind);
 
-    cardHeader.appendChild(cityTitle);
-    cardHeader.appendChild(tempToggle);
-    container.appendChild(cardHeader);
-    container.appendChild(desc);
-    container.appendChild(temp);
-    container.appendChild(tempFeelsLike);
-    container.appendChild(humidity);
-    container.appendChild(wind);
-
-    return container;
+  return container;
 }
 
 function convertToFahrenheit(temp) {
-    return (temp * 9) / 5 + 32;
+  return (temp * 9) / 5 + 32;
 }
 
 function setBackground(weather) {
-    if (weather === "clear sky")
-        document.body.style.backgroundImage = "url('assets/clearsky.jpg')";
-    if (weather.includes("clouds"))
-        document.body.style.backgroundImage = "url('assets/clouds.jpg')";
-    if (weather.includes("rain"))
-        document.body.style.backgroundImage = "url('assets/rain.jpg')";
-    if (weather.includes("snow"))
-        document.body.style.backgroundImage = "url('assets/snow.jpg')";
-    if (weather === "mist")
-        document.body.style.backgroundImage = "url('assets/mist.jpg')";
-    if (weather === "thunderstorm")
-        document.body.style.backgroundImage = "url('assets/thunderstorm.jpg')";
+  if (weather === "clear sky")
+    document.body.style.backgroundImage = "url('assets/clearsky.jpg')";
+  if (weather.includes("clouds"))
+    document.body.style.backgroundImage = "url('assets/clouds.jpg')";
+  if (weather.includes("rain"))
+    document.body.style.backgroundImage = "url('assets/rain.jpg')";
+  if (weather.includes("snow"))
+    document.body.style.backgroundImage = "url('assets/snow.jpg')";
+  if (weather === "mist")
+    document.body.style.backgroundImage = "url('assets/mist.jpg')";
+  if (weather === "thunderstorm")
+    document.body.style.backgroundImage = "url('assets/thunderstorm.jpg')";
 }
 
 export function displayErrorMessage() {
-    const searchBar = document.getElementById("searchbar");
-    const errorMessage = document.getElementById("error-message");
+  const searchBar = document.getElementById("searchbar");
+  const errorMessage = document.getElementById("error-message");
 
-    if (searchBar.value.trim() === "") {
-        errorMessage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-        errorMessage.textContent = "Please enter a city name!";
-    } else {
-        errorMessage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-        errorMessage.textContent = "Location was not found";
-    }
+  if (searchBar.value.trim() === "") {
+    errorMessage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+    errorMessage.textContent = "Please enter a city name!";
+  } else {
+    errorMessage.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+    errorMessage.textContent = "Location was not found";
+  }
 
-    function clearErrorMessages() {
-        errorMessage.textContent = "";
-        errorMessage.style.backgroundColor = "transparent";
-        console.clear();
-    }
-    setTimeout(clearErrorMessages, 2500);
+  function clearErrorMessages() {
+    errorMessage.textContent = "";
+    errorMessage.style.backgroundColor = "transparent";
+    console.clear();
+  }
+  setTimeout(clearErrorMessages, 2500);
 }
